0fdf8f9fb4a34a6d4b0c3006e243d18cb49b5c38
Your Name <you@example.com>
2023-03-12 17:06:02 +0530

    update

--- react-routing/src/Reducer/dataReducer.js
+++ react-routing/src/Reducer/dataReducer.js
@@ -1,19 +1,27 @@
-const is = {data:[]}
+const is = { data: [] };
 export const dataReducer = (state = is, action) => {
   console.log(state, action, "Data Reducer");
 
   switch (action.type) {
-    case "addData":{
-        console.log(action, "data reducer");
-        return{
-            ...state,
-            data:[action.payload, ...state.data]
-        }
+    case "addData": {
+      console.log(action, "data reducer");
+
+      return {
+        ...state,
+        data: [action.payload, ...state.data],
+      };
     }
-      
-    
+
+    case "delData": {
+      return {
+        ...state,
+        data: state.data.filter((data) => {
+         return data.id != action.payload;
+        }),
+      };
+    }
+
     default:
       return state;
-    
   }
 };
--- react-routing/src/components/Contact.js
+++ react-routing/src/components/Contact.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import { addData } from "../Action/addData";
 import { useDispatch, useSelector } from "react-redux";
 import shortid from "shortid";
+import { delData } from "../Action/delData";
 const Contact = () => {
   const dispatch = useDispatch();
-  const selector = useSelector((state) => state.data);
-  console.log(selector+'selecter Data');
+  const selector = useSelector((state) => state.data.data);
+  console.log(selector, "selecter Data");
   const [name, setName] = useState("");
   const [mail, setMail] = useState("");
 
   const onClickHandeller = (e) => {
     e.preventDefault();
     dispatch(addData({ name: name, email: mail, id: shortid.generate() }));
+    setName("");
+    setMail("");
   };
 
   return (
@@ -36,6 +39,36 @@ const Contact = () => {
         ></input>
         <button onClick={onClickHandeller}>Add Data</button>
       </form>
+      <div>
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Name</th>
+
+            <th>Mail</th>
+          </thead>
+          <tbody>
+            {selector.map((data) => (
+              <tr>
+                <td>{data.id}</td>
+
+                <td>{data.name}</td>
+
+                <td>{data.email}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      dispatch(delData(data.id));
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
